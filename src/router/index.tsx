13d0f7b75f3e79fd6cfd167d69e029bd3a2f9db3
gosutesu <gosutesu@gmail.com>
2020-03-06 10:05:39 +0900

    first commit

---
+++ src/router/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Top from '../component/pages/Top';
+import Page1 from '../component/pages/Page1';
+import Page2 from '../component/pages/Page2';
+
+const Router : React.FC= ()=> {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path="/" component={Top} />
+        <Route path="/" component={Page1} />
+        <Route path="/" component={Page2} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
